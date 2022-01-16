281e75c6026f96c88950512fc978195b8ed22a9f
JoshPaulie <30572280+JoshPaulie@users.noreply.github.com>
2022-01-16 12:24:16 -0600

    clean up, attempted logging commands

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """ This runs the bot """
 import discord
 import os
-from dotenv import find_dotenv, load_dotenv
+from dotenv import load_dotenv
 from config import console, __version__
 from modules.init_functions import collect_cogs, load_cogs
 
@@ -13,6 +13,16 @@ async def on_ready():
     """Event that runs when bot has successfully connected"""
     console.log(f"{bot.user} has started")
     await bot.change_presence(activity=discord.Game(name=__version__))
+
+
+""" TODO This worked in dpy, unsure what the new implementation is"""
+# @bot.event
+# async def on_command(ctx):
+#     """Log ever command used"""
+#     message = ctx.message
+#     content = message.content
+#     goon = message.author.name
+#     console.log(f"{goon} used {content}")
 
 
 if __name__ == "__main__":
