8aeca2071f43343f9913dde196b05389ed205578
JoshPaulie <30572280+JoshPaulie@users.noreply.github.com>
2022-01-23 00:24:01 -0600

    change warning color

--- modules/cmd_warning.py
+++ modules/cmd_warning.py
@@ -6,6 +6,6 @@ async def development_warning(ctx: discord.ext.commands.Context):
     warn_embed = discord.Embed(
         title="Under construction! 🚧",
         description="This command may not work as expected.",
-        color=discord.Color.blurple(),
+        color=discord.Color.brand_red(),
     )
     await ctx.send(embed=warn_embed, delete_after=8)
