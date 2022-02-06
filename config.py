fc3a597f01e4d932b47c591c3f0f6c8d62b4f5a2
JoshPaulie <30572280+JoshPaulie@users.noreply.github.com>
2022-02-06 12:59:48 -0600

    ➕ added env_name check

--- config.py
+++ config.py
@@ -1,10 +1,19 @@
 """ Config file """
 
+import os
+
+import dotenv
 from rich.console import Console
 
 __version__ = "Beta-1.1"
+dotenv.load_dotenv()
+
 
 console = Console()
 dev_server = 510865274594131968
 goon_server = 177125557954281472
 all_servers = [dev_server, goon_server]
+
+
+if os.getenv("ENV_NAME") == "DEV":
+    all_servers.remove(goon_server)
