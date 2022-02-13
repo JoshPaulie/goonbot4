98bbd790b2626d07dac1c0b83879e5c8be6d336c
JoshPaulie <30572280+JoshPaulie@users.noreply.github.com>
2022-02-13 08:58:28 -0600

    📅 Countdown fix and update!

--- cogs/countdown.py
+++ cogs/countdown.py
@@ -1,9 +1,11 @@
+import datetime
+
 import discord
 import discord.ext.commands
 import dotenv
 from discord.commands import slash_command
 from discord.ext import commands
-from helpers.special_dates import special_events_remaining
+from helpers.special_dates import get_special_events_remaining
 
 dotenv.load_dotenv()
 
@@ -16,6 +18,7 @@ class Countdown(commands.Cog):
     @slash_command()
     async def today(self, ctx: discord.ApplicationContext) -> None:
         """Returns if it's a special day, or when the next one will be"""
+        special_events_remaining = get_special_events_remaining(datetime.date.today())
         next_event, next_event_days = special_events_remaining[0]
         second_event, second_event_days = special_events_remaining[1]
 
@@ -25,6 +28,9 @@ class Countdown(commands.Cog):
         else:
             event_embed.title = f"{next_event} is in {next_event_days} days!"
             event_embed.description = f"...and {second_event} is in {second_event_days}"
+
+        if next_event_days == 1:
+            event_embed.title = f"{next_event} is tomorrow! 🎉"
 
         await ctx.respond(embed=event_embed)  # type: ignore
 
--- helpers/special_dates.py
+++ helpers/special_dates.py
@@ -1,16 +1,16 @@
 import datetime
 
-today = datetime.date.today()
-current_year = today.year
+current_year = datetime.date.today().year
 
 
-def days_since(end_date) -> int:
-    return (end_date - today).days
+def days_since(end_date: datetime.date, today_date: datetime.date) -> int:
+    """Returns days until end_date from today_date. Used later to sort special events remaining in current year."""
+    return (end_date - today_date).days
 
 
 BIRTHDAYS = {
     "Hudson's Birthday": datetime.date(current_year, 2, 14),
-    "Chri's Birthday": datetime.date(current_year, 4, 21),
+    "Chris' Birthday": datetime.date(current_year, 4, 21),
     "Alex's Birthday": datetime.date(current_year, 4, 15),
     "Daniel's Birthday": datetime.date(current_year, 4, 3),
     "Lex's Birthday": datetime.date(current_year, 5, 20),
@@ -32,8 +32,18 @@ HOLIDAYS = {
     "Christmas 🎄": datetime.date(current_year, 12, 25),
 }
 
-special_events = {**BIRTHDAYS, **HOLIDAYS}
-special_events_remaining = {
-    special_event: days_since(date) for special_event, date in special_events.items() if days_since(date) > -1
-}
-special_events_remaining = sorted(special_events_remaining.items(), key=lambda x: x[1])
+SPECIAL_EVENTS = {**BIRTHDAYS, **HOLIDAYS}
+
+
+def get_special_events_remaining(today_date: datetime.date) -> list[tuple[str, int]]:
+    """
+    Returns special events remaining for the current year
+
+    Future Josh/Anyone else reading this: This might look messy but it's needed.
+    Without a fresh datetime object being passed every call, the countdown is stuck at whatever day the bot happened to be restarted
+    """
+    special_events_remaining = {
+        special_event: days_since(date, today_date) for special_event, date in SPECIAL_EVENTS.items() if days_since(date, today_date) > -1
+    }
+    special_events_remaining = sorted(special_events_remaining.items(), key=lambda x: x[1])
+    return special_events_remaining
